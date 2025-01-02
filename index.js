1787a0afa374377262cf95db8ae3e4f52428a113
Ashish Bhattarai <ashishbhattarai49@gmail.com>
2025-01-02 15:41:18 +0530

    promise all concept

--- index.js
+++ index.js
@@ -165,6 +165,54 @@
 //   }
 // }
 
+// ****************************Promise.all********************************************************
+const p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("P1 success");
+  }, 3000);
+});
+const p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    // resolve("P2 success");
+    reject("p2 fails");
+  }, 1000);
+});
+const p3 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("P3 success");
+  }, 2000);
+});
+
+// ************Promise.all**************
+// Promise.all([p1, p2, p3])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.error(err));
+
+// ************Promise.allSettled**************
+// Promise.allSettled([p1, p2, p3])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.error(err));
+
+//   *************Promise.race*************
+// Promise.race([p1, p2, p3])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.error(err));
+
+//   *************Promise.any*************
+// Promise.any([p1, p2, p3])
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => console.error(err));
+
+// **********************Debouncing****************************************************************
+
 // ***********************************************************************************************************************
 // Concept
 
